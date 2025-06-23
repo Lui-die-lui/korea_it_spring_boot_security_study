f6016361142e6e098c23a9960d70ca0e0dcc3416
Lui-die-lui <lsg05h@naver.com>
2025-06-23 20:53:43 +0900

    Feat:first commit

---
+++ src/main/java/com/koreait/spirngSecurityStudy/SpirngSecurityStudyApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.spirngSecurityStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngSecurityStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngSecurityStudyApplication.class, args);
+	}
+
+}
